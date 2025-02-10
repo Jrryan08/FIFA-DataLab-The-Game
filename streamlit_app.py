60c8695dbe68d47506f0e4ebf35121ff20bd4827
Nelson Mbigili <mbigilinelson@gmail.com>
2025-02-10 23:27:56 +0000

    First Push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# App Title
+st.title("Fifa Data Lab - The Game")
+st.image("fifa_logo_image.png")
+st.write("Welcome, it's great to have you!")
+
+# Sidebar Menu
+st.sidebar.title("Menu")
+
+# List of dataset file paths
+data_files = [
+    "Datasets/FIFA17_official_data.csv",
+    "Datasets/FIFA18_official_data.csv",
+    "Datasets/FIFA19_official_data.csv",
+    "Datasets/FIFA20_official_data.csv",
+    "Datasets/FIFA21_official_data.csv",
+    "Datasets/FIFA22_official_data.csv",
+    "Datasets/FIFA23_official_data.csv"
+]
+
+# Dropdown selection for datasets
+selected_file = st.sidebar.selectbox("Select a dataset", data_files)
+
+# Load selected dataset
+df = pd.read_csv(selected_file)
+st.subheader(f"{selected_file} Preview")
+st.dataframe(df.head())

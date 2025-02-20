05e0b0ddbc6b9d798f17092a8045e6ef03647865
Nelson Mbigili <mbigilinelson@gmail.com>
2025-02-20 12:56:48 +0000

    Updating the structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,41 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn # pip install  scikit-learn 
+
+from streamlit_option_menu import option_menu # type: ignore # pip install streamlit-menu
+st.title("FIFA Data Lab - The Game")
+selected = option_menu(menu_title=None,options=["01: Data","02: Viz","03: Pred",],orientation="horizontal")
+
+# Datasets dictionary
+
+datasets = {
+    "FIFA17": "Datasets/FIFA17_official_data.csv",
+    "FIFA18": "Datasets/FIFA18_official_data.csv",
+    "FIFA19": "Datasets/FIFA19_official_data.csv",
+    "FIFA20": "Datasets/FIFA20_official_data.csv",
+    "FIFA21": "Datasets/FIFA21_official_data.csv",
+    "FIFA22": "Datasets/FIFA22_official_data.csv",
+    "FIFA23": "Datasets/FIFA23_official_data.csv",
+}
+if(selected=="01: Data"):
+    st.markdown("### :violet[Data Overview]")
+    st.expander("Show Code")
+    
+
+elif(selected=="02: Viz"):
+    st.markdown("### :violet[Data Visualization]")
+
+
+elif(selected=="03: Pred"):
+    st.markdown("### :violet[Data Prediction]")
+
+
+
+
 
 # App Title
-st.title("Fifa Data Lab - The Game")
-st.image("fifa_logo_image.png")
-st.write("Welcome, it's great to have you!")
 
-# Sidebar Menu
-st.sidebar.title("Menu")
 
-# List of dataset file paths
-data_files = [
-    "Datasets/FIFA17_official_data.csv",
-    "Datasets/FIFA18_official_data.csv",
-    "Datasets/FIFA19_official_data.csv",
-    "Datasets/FIFA20_official_data.csv",
-    "Datasets/FIFA21_official_data.csv",
-    "Datasets/FIFA22_official_data.csv",
-    "Datasets/FIFA23_official_data.csv"
-]
 
-# Dropdown selection for datasets
-selected_file = st.sidebar.selectbox("Select a dataset", data_files)
-
-# Load selected dataset
-df = pd.read_csv(selected_file)
-st.subheader(f"{selected_file} Preview")
-st.dataframe(df.head())
